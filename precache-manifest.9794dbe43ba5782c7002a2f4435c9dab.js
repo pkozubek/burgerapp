a9a635ce8430fda0ab6336aefed54bd5cdeb3e3e
kozi <pkozubek92@gmail.com>
2019-06-08 21:17:11 +0200

    Updates

---
+++ precache-manifest.9794dbe43ba5782c7002a2f4435c9dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerapp/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "999df6ecfe527c77cff6",
+    "url": "/burgerapp/static/js/runtime~main.d6a95fd7.js"
+  },
+  {
+    "revision": "d7fa5610befe9210aa14",
+    "url": "/burgerapp/static/js/main.ae8984ff.chunk.js"
+  },
+  {
+    "revision": "c469e6775d3a0eced365",
+    "url": "/burgerapp/static/js/2.37c7edda.chunk.js"
+  },
+  {
+    "revision": "d7fa5610befe9210aa14",
+    "url": "/burgerapp/static/css/main.0033ff8d.chunk.css"
+  },
+  {
+    "revision": "c4fac1a1524ddc3bdaba4a93767113bd",
+    "url": "/burgerapp/index.html"
+  }
+];
